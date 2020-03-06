a7a7341771d912fc29121e6bf7165216da562183
Hasan Dik <hasandik68@gmail.com>
2020-03-06 11:36:46 +0300

    Assembly

---
+++ emu8086Example/emu8086Example/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace emu8086Example
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(320, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(21, 22);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(398, 457);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ emu8086Example/emu8086Example/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace emu8086Example
+{
+    public partial class Form1 : Form
+    {
+        private static string sIO_FILE;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int gelen = 1;
+            gelen = READ_IO_BYTE(100);
+            if (gelen == 1)
+            {
+                button1.BackColor = Color.Green;
+            }
+            else
+            {
+                button1.BackColor = Color.Red;
+            }
+
+        }
+        public static System.Byte READ_IO_BYTE(long lPORT_NUM)
+        {
+            StringBuilder sTempDir = new StringBuilder(500);
+            string sFilename = null;
+            //you should activate the following codes when change the filepath
+            sIO_FILE = "filepath";
+            sFilename = sIO_FILE;
+            //FileStream rdr = new FileStream(sFilename, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            //rdr.Seek(lPORT_NUM, SeekOrigin.Begin);
+            //int ch = rdr.ReadByte();
+            //rdr.Close();
+            //return (System.Byte)ch;
+            return 0;
+        }
+
+    }
+
+}
